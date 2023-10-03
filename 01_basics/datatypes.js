76d1543d1ff9474316b3d0683a21208118dba2d2
Uday Pandey <144308525+UdayPandey01@users.noreply.github.com>
2023-10-03 10:38:07 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,30 @@
+"Use strict"; // treat all js code as a newer version
+
+// alert = 3+3 // we are using nodejs not browser
+
+console.log(3 
+    + 3)         // code readability should be high
+
+console.log("uday")   
+
+let name = "uday"
+let age = 19
+let isLoggedIn = false
+let sate = null
+
+
+// number => 2 to the power 53 its range
+// bigInt
+// sring => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof name)
+console.log(typeof isLoggedIn)
+console.log(typeof null)           // object datatypes
+console.log(typeof undefined)      // undefined datatype
