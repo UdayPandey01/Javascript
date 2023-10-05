dd8a3d5e922b4e7157e4331bedcc51061f5df021
Uday Pandey <144308525+UdayPandey01@users.noreply.github.com>
2023-10-05 04:58:43 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,28 @@
+// console.log(2 > 2)
+// console.log(2 < 2)
+// console.log(2 == 2)
+// console.log(2 != 2)
+// console.log(2 >= 2)
+// console.log(2 <= 2)
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);          //in comparison(>=,<=) null changes to 0 thats why it will give true
+
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);     //false in all scenarios
+// console.log(undefined <= 0);
+// console.log(undefined < 0);
+
+
+// === => it checks value strictly
+
+console.log("2" == 2); 
+console.log("2" === 2);            //it checks datatypes also
+console.log("2" >= 2); 
