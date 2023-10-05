bb69058a6ae96a5910ee785a59660afcfea5a9e4
Uday Pandey <144308525+UdayPandey01@users.noreply.github.com>
2023-10-05 07:56:52 +0000

    strings

---
+++ 01_basics/Strings.js
@@ -0,0 +1,35 @@
+const name = "Uday"
+const repo = "10"
+
+// console.log(name + repo + "Hello")
+
+// console.log(`My name is ${name} and my repo count is ${repo}`);
+
+const gameName = new String("Uday-Pandey-567")
+
+// console.log(gameName[9]);
+// console.log(gameName.__proto__);           // object 
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('a'));
+
+// const newName = "      Uday      "
+// console.log(newName);
+// console.log(newName.trim());
+
+
+const newString = gameName.substring(0,5)
+// console.log(newString);
+
+const anotherString = gameName.slice(-8, 10)
+// console.log(anotherString);
+
+
+const url = "http://uday.com/Uday$38Pandey"
+// console.log(url.replace('$38','-'))
+// console.log(url.includes('.'))
+
+console.log(gameName.split('-'))
+
