b6172c68efdb38f078f3b9f7636e69f1054e0f21
Uday Pandey <144308525+UdayPandey01@users.noreply.github.com>
2023-10-06 15:05:41 +0000

    functions

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -33,3 +33,42 @@ function loginUserMessage(username = "SAM"){
 }
 // console.log(loginUserMessage("Uday"))
 console.log(loginUserMessage())                //Output: undefined just logged in
+
+
+
+function calculatePriceCart1(num1){
+    return num1
+}
+// console.log(calculatePriceCart1(200, 300, 500, 1000))         //it will return only 200
+
+function calculatePriceCart (...num1){
+    return num1
+}
+// console.log(calculatePriceCart(200, 300, 500, 1000))          // this will return an array of all element
+
+function calculatePriceCart2(val1, val2, ...num1){
+    return num1
+}
+// console.log(calculatePriceCart2(200, 300, 500, 1000))      // thsi will return an array excluded val1 and val2
+
+
+const user = {
+    username: "Uday",
+    price:"999"
+}
+function handleObject(anyobject){
+    // console.log(`username is ${anyobject.username} and the price is ${anyobject.price}`)
+}
+// handleObject(user)
+handleObject({
+    username: "UdayPandey",
+    price: "9999"
+})
+
+
+const myArray = [200, 300, 1000, 400]
+
+function returnArray(getArray){
+    return getArray[1]
+}
+console.log(returnArray(myArray))
