9619aea428b3b2a4d2a521bb34338e5d9ca138c9
Uday Pandey <144308525+UdayPandey01@users.noreply.github.com>
2023-10-07 04:07:00 +0000

    if, else

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,42 @@
+// if
+
+const isUserLogged = true
+
+if (isUserLogged) {
+    
+}
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const temperature = 37
+// if (temperature < 50) {
+//     console.log("Temperature is less than 50")
+// }
+// else{
+// console.log("Temperature is greater than 50")
+// }
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "Fly"
+//     console.log(`User Power: ${power}`)
+// }
+// console.log(`User Power: ${power}`)               //this will show error because of scope
+
+
+// const balance = 1000
+// if (balance < 500) {
+//     console.log("balance is less than 500")
+// } 
+// else if(balance < 750){
+//     console.log("balance is less than 750")
+// }
+// else if(balance < 900){
+//     console.log("balance is less than 900")
+// }
+// else{
+//     console.log("balance is less than 1200")
+// }
+
+
