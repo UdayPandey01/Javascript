df5dd164fccecf852efef415ac5323d29026bee3
Uday Pandey <144308525+UdayPandey01@users.noreply.github.com>
2023-10-07 11:13:48 +0000

    for of

---
+++ 05_iteration/three.js
@@ -0,0 +1,43 @@
+//for of
+
+["", "", ""]
+[{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+for (const num of arr) {
+    // console.log(num)
+}
+
+const greetings = "Hello World"
+for (const greet of greetings) {
+    // console.log(`Each char is ${greet}`)
+}
+
+
+// Maps
+
+const map = new Map
+map.set("IN", "India")
+map.set("USA", "Unites States Of America")
+map.set("Fr", "France")
+map.set("IN", "India")
+
+// console.log(map)
+
+for (const key of map) {
+    // console.log(key)
+}
+
+for (const [key, value] of map) {
+    // console.log(key, ":- ", value)
+}
+
+
+const myObject = {
+    'game1' : 'NFS',
+    'game2' : 'God Of War',
+    'game3' : 'SpiderMan'   
+}
+// for (const obj of myObject) {
+//     console.log(obj)                 //forOf object me kaam ni kr rha
+// }
