fa9a9be03118fc9d2570241cc1b39729731e9dc2
Uday Pandey <144308525+UdayPandey01@users.noreply.github.com>
2023-10-07 07:46:43 +0000

    while, do while

---
+++ 05_iteration/two.js
@@ -0,0 +1,26 @@
+let index = 0
+while (index <= 10) {
+    // console.log(`Value is ${index}`)
+    index = index + 2
+}
+
+
+let myArray = ["Ichigo ", "zoro", "luffy"]
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`)
+    arr++
+}
+
+let score = 1
+do {
+    // console.log(`Score is ${score}`)
+    score++
+} while (score <= 10);
+
+
+let score2 = 11
+do {
+    console.log(`Score is ${score2}`)
+    score++
+} while (score2 <= 10);
